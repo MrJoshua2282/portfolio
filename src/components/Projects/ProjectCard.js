54e1953cbd7950c0b3e1db5fafa7546b0d7498e9
Joshua McCue <email>
2020-07-03 17:12:29 -0400

    improved project gif display

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ProjectCard.scss';
 
 import Logo from '../Logos/Logo';
 
 const ProjectCard = props => {
+    const [showVideo, setShowVideo] = useState(false);
+
     let git = props.gitHub.map(el => {
         return (<a href={el} key={el} target='_blank' rel="noopener noreferrer"><Logo style={{ display: 'inline' }} language='git' /></a>)
     })
-    return (
-        <div className='card-body'>
-            <div>
-                <span className='card-body-image' >{props.image}</span>
-            </div>
-            <div className='card-body-right'>
-                <div className='card-body-project-name'>{props.name}</div>
-                <div className='card-body-project-languages'>{props.languages}</div>
-                <span>{git}</span>
-                <a href={props.viewUrl} target='_blank' rel="noopener noreferrer"><button className='card-button'>View</button></a>
-            </div>
+    let viewUrl;
+    let video = (
+        <div className='video_container'>
+            <video className='center_video' loop muted autoPlay>
+                <source src={props.viewUrl[1]} />
+            </video>
         </div>
+    )
+    if (props.viewUrl[0] === 'video') {
+        viewUrl = (
+            <button onClick={() => setShowVideo(!showVideo)} className='card-button'>
+                View
+            </button>
+        )
+    } else if (props.viewUrl[0] === 'site') {
+        viewUrl = (
+            <a href={props.viewUrl[1]} target='_blank' rel="noopener noreferrer"><button className='card-button'>View</button></a>
+        )
+    }
+    return (
+        <React.Fragment>
+            <div className='card-body'>
+                <div>
+                    <span className='card-body-image' >{props.image}</span>
+                </div>
+                <div className='card-body-right'>
+                    <div className='card-body-project-name'>{props.name}</div>
+                    <div className='card-body-project-languages'>{props.languages}</div>
+                    <span>{git}</span>
+                    {viewUrl}
+                </div>
+            </div>
+            {props.viewUrl[0] === 'video' && showVideo && video}
+        </React.Fragment>
+
     );
 };
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,24 +12,25 @@ import loopTimer from '../../assets/img/timer.png';
 // import drumApp from '../../assets/img/drum.png';
 // import quote from '../../assets/img/quote.png';
 import quiz from '../../assets/img/quiz.png';
-import ebuyGif from '../../assets/img/ebuy-rcording.gif';
+// import ebuyGif from '../../assets/img/ebuy-rcording.gif';
+import ebuyVideo from '../../assets/video/ebuy-rcording.mov';
 
 class Projects extends Component {
     state = {
         projects: [
-            { name: 'ebuy', image: <img className='project-image' src={ebuy} alt='project' />, viewUrl: ebuyGif, gitHub: ['https://github.com/MrJoshua2282/ebuy-front-end', 'https://github.com/MrJoshua2282/ebuy-back-end'], languages: 'Css | React | Node.js | Express | MongoDb | Mongoose' },
+            { name: 'ebuy', image: <img className='project-image' src={ebuy} alt='project' />, viewUrl: ['video', ebuyVideo], gitHub: ['https://github.com/MrJoshua2282/ebuy-front-end', 'https://github.com/MrJoshua2282/ebuy-back-end'], languages: 'Css | React | Node.js | Express | MongoDb | Mongoose' },
 
-            { name: 'Quiz', image: <img className='project-image' src={quiz} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/quiz/', gitHub: ['https://github.com/MrJoshua2282/quiz/tree/master'], languages: 'Sass | React' },
+            { name: 'Quiz', image: <img className='project-image' src={quiz} alt='project' />, viewUrl: ['site', 'https://mrjoshua2282.github.io/quiz/'], gitHub: ['https://github.com/MrJoshua2282/quiz/tree/master'], languages: 'Sass | React' },
 
-            { name: 'Memory Cards', image: <img className='project-image' src={imageMemoryCards} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/memoryCards/', gitHub: ['https://github.com/MrJoshua2282/memoryCards/tree/master'], languages: 'Sass | React' },
+            { name: 'Memory Cards', image: <img className='project-image' src={imageMemoryCards} alt='project' />, viewUrl: ['site', 'https://mrjoshua2282.github.io/memoryCards/'], gitHub: ['https://github.com/MrJoshua2282/memoryCards/tree/master'], languages: 'Sass | React' },
 
-            { name: 'Form Validation', image: <img className='project-image' src={formApp} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/forms/', gitHub: ['https://github.com/MrJoshua2282/forms/tree/master'], languages: 'Css | React' },
+            { name: 'Form Validation', image: <img className='project-image' src={formApp} alt='project' />, viewUrl: ['site', 'https://mrjoshua2282.github.io/forms/'], gitHub: ['https://github.com/MrJoshua2282/forms/tree/master'], languages: 'Css | React' },
 
-            { name: 'Budget App', image: <img className='project-image' src={budgetApp} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/BudgetApp/', gitHub: ['https://github.com/MrJoshua2282/BudgetApp/tree/master'], languages: 'Css | React' },
+            { name: 'Budget App', image: <img className='project-image' src={budgetApp} alt='project' />, viewUrl: ['site', 'https://mrjoshua2282.github.io/BudgetApp/'], gitHub: ['https://github.com/MrJoshua2282/BudgetApp/tree/master'], languages: 'Css | React' },
 
-            { name: 'Weather Forecast', image: <img className='project-image' src={weatherApp} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/weatherForecast/', gitHub: ['https://github.com/MrJoshua2282/weatherForecast/tree/master'], languages: 'Css | React' },
+            { name: 'Weather Forecast', image: <img className='project-image' src={weatherApp} alt='project' />, viewUrl: ['site', 'https://mrjoshua2282.github.io/weatherForecast/'], gitHub: ['https://github.com/MrJoshua2282/weatherForecast/tree/master'], languages: 'Css | React' },
 
-            { name: 'Loop Timer', image: <img className='project-image' src={loopTimer} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/loopTimer/', gitHub: ['https://github.com/MrJoshua2282/loopTimer/tree/master'], languages: 'Css | React' },
+            { name: 'Loop Timer', image: <img className='project-image' src={loopTimer} alt='project' />, viewUrl: ['site', 'https://mrjoshua2282.github.io/loopTimer/'], gitHub: ['https://github.com/MrJoshua2282/loopTimer/tree/master'], languages: 'Css | React' },
 
             // { name: 'Drum Board', image: <img className='project-image' src={drumApp} alt='project' />, viewUrl: 'https://mrjoshua2282.github.io/drumBoard/', gitHub: ['https://github.com/MrJoshua2282/drumBoard/tree/master'], languages: 'Css | React' },
 
